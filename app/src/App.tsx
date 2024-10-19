c4861f9739f4217c353864ae7ed7be02a1370ce3
A01798671 <a01798671@tec.mx>
2024-10-19 13:25:00 -0600

    color postit and route

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,19 @@
 import "./App.css";
-import {Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
-
+import MyProjects from "./pages/MyProjects";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Home />}>
-            <Route index element={
-              <Home />
-            } /> 
-          </Route>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/MyProjects" element={<MyProjects />}>
+          <Route index element={<MyProjects />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
