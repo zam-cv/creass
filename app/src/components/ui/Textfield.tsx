a824f912286a935b3f0bd651365dc0ff4f9c566d
SsebasA <sebastian.antonio1707@gmail.com>
2024-10-19 13:42:45 -0600

    Update: home

---
+++ app/src/components/ui/Textfield.tsx
@@ -0,0 +1,12 @@
+import { Send } from 'lucide-react';
+
+export default function TextField() {
+    return (
+        <div>
+            <input
+                className='rounded-lg'    
+            />
+
+        </div>
+    )
+}
--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,18 +1,21 @@
-import { Settings, Grid2X2 } from 'lucide-react';
+import { Settings, LayoutGrid } from 'lucide-react';
 
 export default function Home() {
     return (
         <div>
             <header>
-                <div>
-                    <Grid2X2 />
-                </div>
-                <div>
-                    <Settings />
+                <div className='flex justify-between'>
+                    <div className='order-first pl-5 pt-3 '>
+                        <LayoutGrid size={35} />
+                    </div>
+                    <div className='order-last pr-5 pt-3'>
+                        <Settings size={35}/>
+                    </div>
                 </div>
             </header>
+            <body>
 
-
+            </body>
         </div>
     )
 }
