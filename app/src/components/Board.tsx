f1edb321c89c5c87d13eda92ae03f768f4230012
SsebasA <sebastian.antonio1707@gmail.com>
2024-10-19 14:15:31 -0600

    Add: Title

--- app/src/components/Board.tsx
+++ app/src/components/Board.tsx
@@ -2,7 +2,8 @@ import TextField from "./ui/Textfield";
 
 export default function Board() {  
     return (
-        <div>
+        <div className="items-center">
+            <h1 className="font-handlee">CreAss</h1>
             <TextField />
         </div>
     )
