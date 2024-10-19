a6307bd0d2b7fe03081d0c612a41e386c0b5c044
SsebasA <sebastian.antonio1707@gmail.com>
2024-10-19 13:12:51 -0600

    Add: Tailwind

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,10 +3,10 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
-function App() {
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
-
-export default App;
