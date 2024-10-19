b3b6ffef972099ab093784367b4f50ccb479c185
SsebasA <sebastian.antonio1707@gmail.com>
2024-10-19 13:12:45 -0600

    Add: Tailwind

---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { Settings, Grid2X2 } from 'lucide-react';
+
+export default function Home() {
+    return (
+        <div>
+            <header>
+                <div>
+                    
+                </div>
+            </header>
+
+
+        </div>
+    )
+}
