a2153cf2ce4603a23a0372e1ebc4d9f3240828be
SsebasA <sebastian.antonio1707@gmail.com>
2024-10-19 21:03:18 -0600

    Add: Tree

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { LayoutGrid } from "lucide-react";
+import { LayoutGrid, Network  } from "lucide-react";
 import MyProjects from "./MyProjects";
 import Board from "../components/Board";
 import CloseButton from "../components/ui/CloseButton";
 import ThemeSwitch from "../components/ui/ThemeSwitch";
 import { ThemeProvider, useTheme } from "../contexts/ThemeContext";
+import TreeComponent from "../components/TreePath";
 
 const HomeContent: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isTreeOpen, setIsTreeOpen] = useState(false);
   const { theme } = useTheme();
   const [selectedProject, setSelectedProject] = useState<string | null>(null);
 
@@ -26,6 +28,15 @@ const HomeContent: React.FC = () => {
     setIsModalOpen(false);
   };
 
+  const handleOpenTree = () => {    
+    setIsTreeOpen(true);
+  }
+
+    const handleCloseTree = () => {
+        setIsTreeOpen(false);
+}
+
+
   return (
     <div
       className={`${
@@ -35,12 +46,21 @@ const HomeContent: React.FC = () => {
       <header className="h-[8vh]">
         <div className="flex justify-between items-center">
           <div className="order-first pl-5 pt-3">
-            <LayoutGrid
-              size={35}
-              onClick={handleOpenModal}
-              className="cursor-pointer"
-              color={theme === "dark" ? "white" : "black"}
-            />
+            <div className="flex items-center justify-center space-x-2">
+                <LayoutGrid
+                size={35}
+                onClick={handleOpenModal}
+                className="cursor-pointer"
+                color={theme === "dark" ? "white" : "black"}
+                /> 
+                <Network 
+                size = {35}
+                onClick={handleOpenTree}
+                className="cursor-pointer"
+                color={theme === "dark" ? "white" : "black"}
+                    />
+            </div>
+
           </div>
           <div className="flex items-center justify-center mt-2">
             <h1
@@ -52,6 +72,7 @@ const HomeContent: React.FC = () => {
             </h1>
           </div>
           <div className="order-last pr-5 pt-3">
+            
             <ThemeSwitch />
           </div>
         </div>
@@ -70,6 +91,18 @@ const HomeContent: React.FC = () => {
           >
             <CloseButton onClick={handleCloseModal} />
             <MyProjects />
+          </div>
+        </div>
+      )}
+      {isTreeOpen && (
+        <div className="fixed w-full inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <div
+            className={`relative ${
+              theme === "dark" ? "bg-darkMode" : "bg-gray-300"
+            } p-10 rounded-lg w-3/4 h-3/4`}
+          >
+            <CloseButton onClick={handleCloseTree} />
+            <TreeComponent />
           </div>
         </div>
       )}
